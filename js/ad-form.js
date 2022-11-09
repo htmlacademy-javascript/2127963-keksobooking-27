69ebaba75673bf3d9f82a1df32e4bc6d4e4595ae
Elvira <elveechernenko@yandex.ru>
2022-11-09 23:55:34 +0300

    Добавляет валидацию заголовка с помощью Pristine

---
+++ js/ad-form.js
@@ -0,0 +1,26 @@
+const adForm = document.querySelector('.ad-form');
+
+
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element--invalid',
+  successClass: 'ad-form__element--valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'text-help'
+});
+
+function validateTitle (value) {
+  return value.length >= 30 && value.length <= 100;
+}
+
+pristine.addValidator(
+  adForm.querySelector('#title'),
+  validateTitle,
+  'От 30 до 100 символов.'
+);
+
+adForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
