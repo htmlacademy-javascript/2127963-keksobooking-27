91a09eb683d761298187e8611dfa638966f3f26a
Elvira <elveechernenko@yandex.ru>
2022-11-11 08:51:18 +0300

    Добавляет валидацию полей Заголовок, Цена, Кол-во комнат и мест

--- js/ad-form.js
+++ js/ad-form.js
@@ -7,18 +7,48 @@ const pristine = new Pristine(adForm, {
   successClass: 'ad-form__element--valid',
   errorTextParent: 'ad-form__element',
   errorTextTag: 'span',
-  errorTextClass: 'text-help'
-});
+  errorTextClass: 'text-help',
+},
+true
+);
 
-function validateTitle (value) {
-  return value.length >= 30 && value.length <= 100;
+// Валидация количества комнат и гостей
+const roomsField = adForm.querySelector('[name="rooms"]');
+const capacityField = adForm.querySelector('[name="capacity"]');
+
+const roomsOption = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['0'],
+};
+
+function validateCapacity () {
+  return roomsOption[roomsField.value].includes(capacityField.value);
+}
+function getCapacityErrorMessage () {
+  return 'Вариант недоступен для выбранного количества комнат';
 }
 
-pristine.addValidator(
-  adForm.querySelector('#title'),
-  validateTitle,
-  'От 30 до 100 символов.'
-);
+function getRoomsErrorMessage () {
+  return 'Вариант недоступен для выбранного количества гостей';
+}
+
+function onCapacityChange () {
+  pristine.validate(capacityField);
+  pristine.validate(roomsField);
+}
+
+function onRoomsChange () {
+  pristine.validate(capacityField);
+  pristine.validate(roomsField);
+}
+
+pristine.addValidator(roomsField, validateCapacity, getRoomsErrorMessage);
+pristine.addValidator(capacityField, validateCapacity, getCapacityErrorMessage);
+
+roomsField.addEventListener('change', onRoomsChange);
+capacityField.addEventListener('change', onCapacityChange);
 
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
